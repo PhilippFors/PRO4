48c78ef9fb20330433e0f06d469d8beeb5016fee
Kipepo <thomas1.fuerst@gmail.com>
2020-03-13 22:56:56 +0100

    Audio Reaktivitäts Prototyp Leider große Datei da das FMOD Plugin leider so riesig ist und ich wegen meine Unwissenheit nicht sicher bin ob ich es weglassen kann.

---
+++ Assets/Scripts/AudioScripts/FMODScripts/AudioCube.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCube : MonoBehaviour
+{
+   
+    public int[] _bandArray;
+   // public float _startScale, _maxScale;
+    public bool _useBuffer;
+
+    public bool _deathBlock;
+
+    public bool _colorCube;
+
+    public bool _yDynamicCube;
+    public float _yDynamicFactor;
+
+    public bool _useUnityAudioSystem;
+
+
+    int _bandAmount;
+    Material[] _materials;
+    Material[] _arMaterials;
+
+    Color color1;
+    Color color2;
+
+    Color[] colorArray;
+    int hitCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO 
+        //Einbauen dass nur Materials mit den Anfang ar in die Liste aufgenommen werden
+
+
+        _bandAmount = _bandArray.Length;
+       
+        colorArray = new Color[_bandAmount];
+        _materials = GetComponent<MeshRenderer>().materials;
+        _arMaterials = new Material[_materials.Length];
+
+        int j = 0;
+        for (int i = 0; i < _materials.Length; i++)
+        {
+          
+            if (_materials[i].name.Substring(0, 2) == "ar")
+            {
+                _arMaterials[j] = _materials[i];
+                j++;
+            }
+
+        }
+
+
+
+        for (int i = 0; i < _bandAmount; i++)
+        {
+           if (_colorCube)
+            {
+                    colorArray[i] = _arMaterials[i].GetColor("_EmissionColor");
+            }
+           
+        }
+        hitCounter = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_useBuffer)
+        {
+            if (_useUnityAudioSystem)
+            {
+                float H, S, V;
+                for (int i = 0; i < _bandAmount; i++)
+                {
+                    Color.RGBToHSV((colorArray[i]), out H, out S, out V);
+                    V = AudioListenerPeer._audioBandBuffer[_bandArray[i]];
+                    _arMaterials[i].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+                }
+            }
+           else
+            {
+                if (_yDynamicCube)
+                {
+                    if (FMODAudioPeer._audioBandBuffer[_bandArray[0]] >= 0)
+                    {
+                        transform.localScale = new Vector3(transform.localScale.x, FMODAudioPeer._audioBandBuffer[_bandArray[0]] * _yDynamicFactor, transform.localScale.z);
+                    }
+                   
+                }
+                
+                if (_colorCube)
+                {
+                    // NEW IMPLEMENTATION WITH FMOD AUDIO SYSTEm
+                    float H, S, V;
+                    for (int i = 0; i < _bandAmount; i++)
+                    {
+                        Color.RGBToHSV((colorArray[i]), out H, out S, out V);
+                        V = FMODAudioPeer._audioBandBuffer[_bandArray[i]];
+                        _arMaterials[i].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+                    }
+                }
+            }
+        }
+        //IF NOT USE BUFFER
+        else
+        {
+            // NEW IMPLEMENTATION WITH FMOD AUDIO SYSTEm
+            float H, S, V;
+            for (int i = 0; i < _bandAmount; i++)
+            {
+                Color.RGBToHSV((colorArray[i]), out H, out S, out V);
+                V = FMODAudioPeer._audioBand[_bandArray[i]];
+                _arMaterials[i + 1].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+            }
+
+            //DEATH CUBE
+            /*
+             if (V > 0.7f)
+             {
+                 hitCounter++;
+                 _material[1].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+                 _material[2].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+             }
+             else
+             {
+                 _material[1].SetColor("_EmissionColor", Color.black);
+                 _material[2].SetColor("_EmissionColor", Color.black);
+             }
+
+             //Color _color2 = new Color(AudioListenerPeer._audioBandBuffer[_band2], AudioListenerPeer._audioBandBuffer[_band2], AudioListenerPeer._audioBandBuffer[_band2]);
+
+             */
+        }
+    }
+}
+
---
+++ Assets/Scripts/AudioSkillsController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSkillsController : MonoBehaviour
+{
+
+    public GameObject AudioPeer;
+    FMODUnity.StudioEventEmitter emitter;
+
+    bool lowPassActive;
+    bool highPassActive;
+    bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowPassActive = false;
+        highPassActive = false;
+        emitter = AudioPeer.GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1") && !highPassActive)
+        {
+            Debug.Log("Pressed 1");
+            if (!lowPassActive)
+            {
+                
+                lowPassActive = true; 
+                emitter.SetParameter("LowPass", 0.3f);
+            }
+            else
+            {
+                lowPassActive = false;
+                emitter.SetParameter("LowPass", 1f);
+            }
+        }
+
+        if (Input.GetKeyDown("2") && !lowPassActive)
+        {
+            emitter = AudioPeer.GetComponent<FMODUnity.StudioEventEmitter>();
+            if (!highPassActive)
+            {
+                highPassActive = true;
+                emitter.SetParameter("HighPass", 0.5f);
+            }
+            else
+            {
+                highPassActive = false;
+                emitter.SetParameter("HighPass", 1f);
+            }
+        }
+    }
+}
