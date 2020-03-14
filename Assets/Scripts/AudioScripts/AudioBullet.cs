83274ccd53eff7f79e5b10f0460bae5386bb2cd2
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-03-14 23:54:26 +0100

    idk what I'm doing

---
+++ Assets/Scripts/AudioScripts/AudioBullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBullet : MonoBehaviour
+{
+    public int _band1;
+    public bool _useBuffer;
+    Material[] _material;
+
+    Color color1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = GetComponent<MeshRenderer>().materials;
+        color1 = _material[0].GetColor("_EmissionColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_useBuffer)
+        {
+
+            //float test = (AudioPeer._audioBandBuffer[_band2] * _maxScale) + _startScale;
+            // Debug.Log(test, test);
+            //transform.localScale = new Vector3(transform.localScale.x, AudioPeer._audioBand[_band2], transform.localScale.z);
+
+
+            float H, S, V;
+            Color.RGBToHSV((color1), out H, out S, out V);
+            V = AudioListenerPeer._audioBandBuffer[_band1];
+
+          
+            _material[0].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+
+
+
+        }
+
+        else
+        {
+            // transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._audioBand[_band] * _maxScale) + _startScale, transform.localScale.z);
+
+            float H, S, V;
+            Color.RGBToHSV((color1), out H, out S, out V);
+            V = AudioListenerPeer._audioBand[_band1];
+
+           
+            _material[0].SetColor("_EmissionColor", Color.HSVToRGB(H, S, V, true));
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/DeathBlockTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBlockTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+     //   GetComponent<MeshRenderer>().materials;
+
+        //GameManager.instance.restart();
+       // if (GameManager.instance.owlDead)
+      //  {
+           // Destroy(this.gameObject);
+      //  }
+
+    }
+}
