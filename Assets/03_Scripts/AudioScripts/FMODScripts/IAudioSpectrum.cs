96811aaa8c28759e3591072f025b559d9d069425
Kipepo <thomas1.fuerst@gmail.com>
2020-04-19 00:20:45 +0200

    entfernen der Tileblocks da die aufgrund der HDRP nicht mehr funktionieren und sowieso gaga sind, bloom der camera volume hinzugefügt, track ausgetauscht

---
+++ Assets/03_Scripts/AudioScripts/FMODScripts/IAudioSpectrum.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAudioSpectrum
+{
+    // Start is called before the first frame update
+
+
+    float getFqBandBuffer8(int id);
+
+
+    float getFqBand8(int id);
+
+
+    float getAmplitudeBuffer();
+
+
+    float getFqBandBuffer32(int id);
+
+
+    float getFqBand32(int id);
+
+
+}
