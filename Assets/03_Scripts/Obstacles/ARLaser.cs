cbc5449c28be098e9198e1d9d8fe81ef3447650c
Kipepo <thomas1.fuerst@gmail.com>
2020-04-19 23:48:13 +0200

    Anpassungen an FMOD Script

--- Assets/03_Scripts/Obstacles/ARLaser.cs
+++ Assets/03_Scripts/Obstacles/ARLaser.cs
@@ -9,8 +9,8 @@ public class ARLaser : MonoBehaviour
     public float _scale;
     public float _threshold;
 
-    Vector3 startScale = new Vector3(1f, 1f, 1f);
-    Vector3 newScale = new Vector3(1f, 10f, 1f);
+    Vector3 startScale = new Vector3(3f, 3f, 3f);
+    Vector3 newScale = new Vector3(3f, 10f, 3f);
     float speed = 0.5f;
 
     // Start is called before the first frame update
@@ -24,15 +24,18 @@ public class ARLaser : MonoBehaviour
     {
         float fqValueHard = 0;
         float fqValueBuffer = 0;
-        bool beatFull = FMODAudioPeer._instance.getBeatFull();
-        if (beatFull)
+        bool beatFulld2 = false;
+        if (beatFulld2)
         {
-            transform.localScale = Vector3.Lerp(startScale, newScale, 1000* Time.deltaTime);
-            Debug.Log("BARFULL");
+
+            transform.localScale = newScale;
+         
+
         }
         else
         {
-            transform.localScale = Vector3.Lerp(newScale, startScale, 1000 * Time.deltaTime);
+            Vector3 currentScale = transform.localScale;
+            transform.localScale = Vector3.Lerp(currentScale, startScale, 10 * Time.deltaTime);
         }
         //Debug.Log("FQVALUE" + fqValueHard);
     }
