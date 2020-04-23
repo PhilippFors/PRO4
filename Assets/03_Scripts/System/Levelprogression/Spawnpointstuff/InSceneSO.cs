ce618e1512afbc4e226c19f2cac9a257673493b6
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-04-23 21:49:08 +0200

    Spawnpoint prototpyes
    Scriptable object is instantiated at runtime so scene objects can be used

---
+++ Assets/03_Scripts/System/Levelprogression/Spawnpointstuff/InSceneSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class InSceneSO : MonoBehaviour
+{
+  public Level level;
+    public void OnEnable()
+    {
+        if (level == null)
+            level = ScriptableObject.CreateInstance<Level>();
+    }
+}
--- Assets/03_Scripts/System/Levelprogression/Spawnpointstuff/Level.cs
+++ Assets/03_Scripts/System/Levelprogression/Spawnpointstuff/Level.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// [CreateAssetMenu(menuName = "SpawnStuff/Level")]
-[System.Serializable]
-public class Level
+[CreateAssetMenu(menuName = "SpawnStuff/Level")]
+// [System.Serializable]
+public class Level:ScriptableObject
 {
     public int lvlID;
     public Area[] areas;
