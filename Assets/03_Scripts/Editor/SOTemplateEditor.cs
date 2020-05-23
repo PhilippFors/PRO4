030906455b57ce8db1b2f2537e155b58db58e592
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-05-23 17:06:33 +0200

    Finding all templates possible in editor

---
+++ Assets/03_Scripts/Editor/SOTemplateEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SOTemplateEditor : EditorWindow
+{
+    bool init = false;
+    List<StatTemplate> templates = new List<StatTemplate>();
+    string[] paths;
+    string[] stuff = new string[10];
+    [MenuItem("Tools/Custom Window/Template Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow<SOTemplateEditor>("Template Editor");
+    }
+
+    void OnGUI()
+    {
+
+        paths = AssetDatabase.FindAssets("t:StatTemplate");
+        for (int i = 0; i < paths.Length; i++)
+        {
+            stuff[i] = AssetDatabase.GUIDToAssetPath(paths[i]);
+        }
+
+        if (!init)
+        {
+            for (int i = 0; i < paths.Length; i++)
+                templates.Add((StatTemplate)AssetDatabase.LoadAssetAtPath(stuff[i], typeof(StatTemplate)));
+            init = true;
+        }
+
+
+        foreach (StatTemplate t in templates)
+            EditorGUILayout.ObjectField(t, typeof(StatTemplate));
+
+
+    }
+
+}
