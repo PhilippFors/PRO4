023b4bd8a7ce3b84fc5635bcbbe1854246307467
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-06-22 22:20:58 +0200

    Extra State Editor Stuff

--- Assets/03_Scripts/Editor/StateEditor.cs
+++ Assets/03_Scripts/Editor/StateEditor.cs
@@ -9,9 +9,10 @@ public class StateEditor : EditorWindow
     string[] paths;
     Vector2 scrollPosition = Vector2.zero;
     List<State> states = new List<State>();
-
+    List<bool> stateBools = new List<bool>();
+    List<bool> transitionBools = new List<bool>();
     float maxWidth = 320f;
-
+    bool stateCollapse;
     bool choosen = true;
 
     [MenuItem("Tools/Custom Window/State Editor")]
@@ -29,6 +30,7 @@ public class StateEditor : EditorWindow
 
         if (!choosen)
         {
+            stateBools = new List<bool>();
             states = new List<State>();
             string[] guid = Selection.assetGUIDs;
             folderPath = new string[guid.Length];
@@ -43,53 +45,86 @@ public class StateEditor : EditorWindow
             }
 
             for (int i = 0; i < paths.Length; i++)
+            {
                 states.Add((State)AssetDatabase.LoadAssetAtPath(paths[i], typeof(State)));
+                stateBools.Add(false);
+                transitionBools.Add(true);
+            }
             choosen = true;
         }
 
-        if (states.Count == 0)
+        if (states.Count == 0 || states == null)
             return;
         scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+        int j = 0;
         foreach (State t in states)
         {
-            GUILayout.Label(t.name, EditorStyles.largeLabel);
-            EditorGUILayout.ObjectField(t, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
-            EditorGUILayout.Separator();
-            var so = new SerializedObject(t);
-
-
-            int actnr = 0;
-            for (int i = 0; i < t.actions.Length; i++)
+            stateBools[j] = EditorGUILayout.Foldout(stateBools[j], t.name);
+            if (stateBools[j])
             {
-                GUILayout.Label("Actions ", EditorStyles.boldLabel);
-                var ab = so.FindProperty("actions").GetArrayElementAtIndex(i).objectReferenceValue = EditorGUILayout.ObjectField("Action " + actnr, t.actions[i], typeof(Action), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
-                so.ApplyModifiedProperties();
-                actnr++;
-            }
-
-            GUILayout.Label("____________", EditorStyles.boldLabel);
-
-            int trnr = 0;
-            for (int i = 0; i < t.transitions.Length; i++)
-            {
-                var tra = so.FindProperty("transitions").GetArrayElementAtIndex(i);
-                GUILayout.Label("Transition " + trnr, EditorStyles.boldLabel);
-                var d = tra.FindPropertyRelative("decision").objectReferenceValue = EditorGUILayout.ObjectField("Decision", t.transitions[i].decision, typeof(Decision), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                EditorGUILayout.ObjectField(t, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
                 EditorGUILayout.Separator();
-                var s = tra.FindPropertyRelative("trueState").objectReferenceValue = EditorGUILayout.ObjectField("True State", t.transitions[i].trueState, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
-                var f = tra.FindPropertyRelative("falseState").objectReferenceValue = EditorGUILayout.ObjectField("False State", t.transitions[i].falseState, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                var so = new SerializedObject(t);
+
+                int actnr = 1;
+                GUILayout.Label("Actions ", EditorStyles.boldLabel);
+                for (int i = 0; i < t.actions.Length; i++)
+                {
+                    var ab = so.FindProperty("actions").GetArrayElementAtIndex(i).objectReferenceValue = EditorGUILayout.ObjectField("Action " + actnr, t.actions[i], typeof(Action), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                    so.ApplyModifiedProperties();
+                    actnr++;
+                }
+                if (GUILayout.Button("Add Action"))
+                {
+                    so.FindProperty("actions").InsertArrayElementAtIndex(t.actions.Length);
+                    so.ApplyModifiedProperties();
+                }
+                if (GUILayout.Button("Remove Action"))
+                {
+                    so.FindProperty("actions").GetArrayElementAtIndex(t.actions.Length - 1).objectReferenceValue = null;
+                    so.FindProperty("actions").DeleteArrayElementAtIndex(t.actions.Length - 1);
+                    so.ApplyModifiedProperties();
+                }
+                EditorGUILayout.Separator();
+
+                transitionBools[j] = EditorGUILayout.Foldout(transitionBools[j], "Transitions");
+                if (transitionBools[j])
+                {
+                    int trnr = 1;
+                    for (int i = 0; i < t.transitions.Length; i++)
+                    {
+                        var tra = so.FindProperty("transitions").GetArrayElementAtIndex(i);
+                        GUILayout.Label("Transition " + trnr, EditorStyles.boldLabel);
+                        var d = tra.FindPropertyRelative("decision").objectReferenceValue = EditorGUILayout.ObjectField("Decision", t.transitions[i].decision, typeof(Decision), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                        var s = tra.FindPropertyRelative("trueState").objectReferenceValue = EditorGUILayout.ObjectField("True State", t.transitions[i].trueState, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                        var f = tra.FindPropertyRelative("falseState").objectReferenceValue = EditorGUILayout.ObjectField("False State", t.transitions[i].falseState, typeof(State), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                        so.ApplyModifiedProperties();
+                        EditorGUILayout.Separator();
+
+                        trnr++;
+                    }
+                    if (GUILayout.Button("Add Transition"))
+                    {
+                        so.FindProperty("transitions").InsertArrayElementAtIndex(t.transitions.Length);
+                        so.ApplyModifiedProperties();
+                    }
+                    if (GUILayout.Button("Remove Transition"))
+                    {
+                        so.FindProperty("transitions").GetArrayElementAtIndex(t.transitions.Length - 1).objectReferenceValue = null;
+                        so.FindProperty("transitions").DeleteArrayElementAtIndex(t.transitions.Length - 1);
+                        so.ApplyModifiedProperties();
+                    }
+                }
+                EditorGUILayout.Separator();
+
+                so.FindProperty("onExitState").objectReferenceValue = EditorGUILayout.ObjectField("On Exit", t.onExitState, typeof(OnExitState), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+                so.FindProperty("onEnterState").objectReferenceValue = EditorGUILayout.ObjectField("On Enter", t.onEnterState, typeof(OnEnterState), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
+
+                EditorGUILayout.Separator();
+
                 so.ApplyModifiedProperties();
-                GUILayout.Label("____________", EditorStyles.boldLabel);
-
-                trnr++;
             }
-
-            so.FindProperty("onExitState").objectReferenceValue = EditorGUILayout.ObjectField("On Exit", t.onExitState, typeof(OnExitState), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
-            so.FindProperty("onEnterState").objectReferenceValue = EditorGUILayout.ObjectField("On Enter", t.onEnterState, typeof(OnEnterState), false, GUILayout.MinWidth(200f), GUILayout.MaxWidth(maxWidth));
-
-            GUILayout.Label("____________", EditorStyles.boldLabel);
-            EditorGUILayout.Separator();
-            so.ApplyModifiedProperties();
+            j++;
         }
 
         GUILayout.EndScrollView();
