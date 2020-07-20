cf8d74c2e1377b9b956f674d332d717acc4b3c23
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-07-21 01:07:32 +0200

    Avik (Durga) Hitboxes and Weapon script
    Technically should do damage. Haven't tested it.

---
+++ Assets/03_Scripts/Entities/Enemy/Avik/AvikWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvikWeapon : MonoBehaviour
+{
+    public StateMachineController controller;
+    BoxCollider col => GetComponent<BoxCollider>();
+
+    private void Update()
+    {
+        switch (controller.isAttacking)
+        {
+            case true:
+                col.enabled = true;
+                break;
+            case false:
+                col.enabled = false;
+                break;
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (controller.isAttacking)
+        {
+            if (!obj.GetComponent<EnemyBody>() & obj.GetComponent<IHasHealth>() != null)
+            {
+                EventSystem.instance.OnAttack(obj.GetComponent<IHasHealth>(), controller.enemyStats.GetStatValue(StatName.BaseDmg));
+            }
+        }
+    }
+}
