35f1e08eef2b1e69b5fbb0d3a7486c5cdd9082a9
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-08-09 17:44:25 +0200

    Camera controller changes

--- Assets/03_Scripts/Entities/Player/CameraController.cs
+++ Assets/03_Scripts/Entities/Player/CameraController.cs
@@ -6,18 +6,48 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject player => GameObject.FindGameObjectWithTag("Player");
     private Vector3 playerPosition;
-    
+    public Camera mainCam;
     private PlayerStateMachine playercontrols;
     public float cameraFollowSpeed = 5f;
+    float width = Screen.width;
+    float height = Screen.height;
+    float oldX = 0;
+    float oldY = 0;
 
-    private void Start() {
+    public float targetBias = 0.5f;
+    private void Start()
+    {
         playercontrols = player.GetComponent<PlayerStateMachine>();
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
     }
     private void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z) + playercontrols.currentMoveDirection* 1.05f;
-        // transform.position = Vector3.Lerp(transform.position, playerPosition, Time.fixedDeltaTime*cameraFollowSpeed);
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        Vector3 s = getDifference();
+        s.y = player.transform.position.y;
+        transform.position = Vector3.Lerp(transform.position, s, Time.deltaTime * cameraFollowSpeed);
     }
+
+    Vector3 getDifference()
+    {
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, player.transform.position.y, 0));
+        //creating the Ray
+        Ray cameraRay = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        float rayLength;
+
+        //checking if the raycast intersects with the plane
+        oldX = Input.mousePosition.x;
+        oldY = Input.mousePosition.y;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 rayPoint = cameraRay.GetPoint(rayLength);
+
+            Vector3 dist = rayPoint - player.transform.position;
+            return player.transform.position + dist * targetBias;
+        }
+
+        return player.transform.position;
+    }
+
 }
