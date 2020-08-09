ef0bb4452939f1882a749fec15be4143768926ca
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-08-09 17:44:09 +0200

    Some Lighting and particle stuff, added stuff to background

--- Assets/03_Scripts/System/ParticleController.cs
+++ Assets/03_Scripts/System/ParticleController.cs
@@ -22,10 +22,12 @@ public class ParticleController : MonoBehaviour
 
     void StartParticles(Vector3[] points)
     {
+        Vector3 dir = particlePoints[0].position - points[0];
+        dir.y = 0;
         for (int i = 0; i < particlePoints.Length; i++)
         {
-            Instantiate(hitParticle, points[i], new Quaternion(0, 0, 0, 0));
-            Instantiate(electricity, points[i], new Quaternion(0, 0, 0, 0));
+            Instantiate(hitParticle, points[i], new Quaternion(0,0,0,0));
+            Instantiate(electricity, points[i], Quaternion.Euler(dir));
         }
     }
 }
